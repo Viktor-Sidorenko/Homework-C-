e4692f572d1a38cf8c1e2e12648d28fb2b57505b
Viktor <dark.player921@mail.ru>
2022-12-03 17:49:13 +0800

    Сделал вторую задачу

--- Lesson001/Exercise002/Program.cs
+++ Lesson001/Exercise002/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(1,10);
+Console.WriteLine(number);
+double result = number % 2;
+if (result == 0) 
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
