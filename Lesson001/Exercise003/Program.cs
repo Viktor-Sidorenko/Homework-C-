44830b0f03eb4a5ab3f4bd97f9faa6939a5ba0bf
Viktor <dark.player921@mail.ru>
2022-12-03 18:52:36 +0800

    Сделал третью задачу

--- Lesson001/Exercise003/Program.cs
+++ Lesson001/Exercise003/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(1,10);
+Console.WriteLine(number);
+
+int count = 2;
+while (number >= count)
+{
+    Console.WriteLine(count);
+    count = count + 2;
+}
